4a8646e06093b2d4173d46c06eb5d4e2ea2323af
Kibasa <miguelhmsantos21@gmail.com>
2024-05-24 22:16:30 -0300

    another add

--- src/components/Header/Cart.jsx
+++ src/components/Header/Cart.jsx
@@ -1,7 +1,51 @@
-export default function Cart() {
-    return(
-        <p>
-            <button>Cart</button>
-        </p>
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
+
+const Cart = forwardRef(function Cart({ }, ref) {
+
+    const dialog = useRef();
+
+    const isModalOpen = useState(false);
+
+    useImperativeHandle(ref, () => {
+        return {
+            open: () => {
+                dialog.current.showModal();
+            }
+        };
+    });
+
+    return (
+        <dialog ref={dialog} className="modal">
+            <h2 className="cart">Your Table</h2>
+            <li className="cart-item"> {/* Organizar */}
+                <div className="cart-item">
+                    <span>Snackzin - 12 pau</span>
+                </div>
+                <div className="cart-item-actions">
+                    <button>-</button>
+                    <span>1</span>
+                    <button>+</button>
+                </div>
+            </li>
+            <li className="cart-item">
+                <div className="cart-item">
+                    <span>Snackzao - 30 pau</span>
+                </div>
+                <div className="cart-item-actions">
+                    <button>-</button>
+                    <span>2</span>
+                    <button>+</button>
+                </div>
+            </li>
+            <p className="cart-total">
+                <strong>R$ 42 pau</strong>
+            </p>
+            <div className="modal-actions">
+                <button className="text-button" onClick={() => dialog.current.close()}>Close</button>
+                <button className="button">Go to Checkout</button>
+            </div>
+        </dialog>
     );
-}
+})
+
+export default Cart;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,26 @@
+import { useRef } from "react";
 import Cart from "./Cart";
 
 export default function Header() {
+
+    const modal = useRef();
+
+    function handleOpenCartClick() {
+        modal.current.open();
+    }
+
     return (
-        <header id="main-header">
-            <div id="title">
-                <img src="logo.jpg" />
-                <h1>REACTFOOD</h1>
-            </div>
-            <Cart />
-        </header>
+        <>
+            <Cart ref={modal} />
+            <header id="main-header">
+                <div id="title">
+                    <img src="logo.jpg" />
+                    <h1>MIGUEL'S FOOD</h1>
+                </div>
+                <p>
+                    <button className="text-button" onClick={handleOpenCartClick}>Cart</button>
+                </p>
+            </header>
+        </>
     );
 }
